15610833bf9e295dfb3aa7fc17f2f77117627eea
sasuke-7 <davidkunschventurini@gmail.com>
2021-09-03 21:14:35 -0300

    chore: add dotenv & .env example

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import 'dotenv/config'
+
 import { Bot } from '@client'
 import { Intents } from 'discord.js'
 
 const client = new Bot({
   token: process.env.DISCORD_TOKEN,
-  intents: Object.values(Intents).reduce((a, b) => a + b),
+  intents: Object.values(Intents.FLAGS).reduce((a, b) => a + b),
 })
 
 client.start()
